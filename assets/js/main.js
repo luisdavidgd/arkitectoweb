e3bff5268aace6674276dfb34afe9d7193374520
Luda <luisdavidgd@gmail.com>
2019-03-12 17:02:07 -0500

    React added

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,21 @@
 			scrollTop: $(this.hash).offset().top
 		}, 600);
 	});
+	$("#nav .navbar-brand a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
+
+	$("#home .home-content a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
 
 	$('#back-to-top').on('click', function(){
 		$('body,html').animate({

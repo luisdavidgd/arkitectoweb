fbb73abf649e8543e0e03cc83c455ad2d630c64a
Luda <luisdavidgd@gmail.com>
2019-03-13 15:20:00 -0500

    Re-order files again

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-(function($) {
+(function ($) {
 	"use strict"
 
 	///////////////////////////
 	// Preloader
-	$(window).on('load', function() {
+	$(window).on('load', function () {
 		$("#preloader").delay(600).fadeOut();
 	});
 
@@ -16,22 +16,14 @@
 
 	///////////////////////////
 	// Smooth scroll
-	$("#nav .main-nav a[href^='#']").on('click', function(e) {
+	$("#nav .main-nav a[href^='#']").on('click', function (e) {
 		e.preventDefault();
 		var hash = this.hash;
 		$('html, body').animate({
 			scrollTop: $(this.hash).offset().top
 		}, 600);
 	});
-	$("#nav .navbar-brand a[href^='#']").on('click', function(e) {
-		e.preventDefault();
-		var hash = this.hash;
-		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top
-		}, 600);
-	});
-
-	$("#home .home-content a[href^='#']").on('click', function(e) {
+	$("#nav .navbar-brand a[href^='#']").on('click', function (e) {
 		e.preventDefault();
 		var hash = this.hash;
 		$('html, body').animate({
@@ -39,7 +31,15 @@
 		}, 600);
 	});
 
-	$('#back-to-top').on('click', function(){
+	$("#home .home-content a[href^='#']").on('click', function (e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
+
+	$('#back-to-top').on('click', function () {
 		$('body,html').animate({
 			scrollTop: 0
 		}, 600);
@@ -47,19 +47,19 @@
 
 	///////////////////////////
 	// Btn nav collapse
-	$('#nav .nav-collapse').on('click', function() {
+	$('#nav .nav-collapse').on('click', function () {
 		$('#nav').toggleClass('open');
 	});
 
 	///////////////////////////
 	// Mobile dropdown
-	$('.has-dropdown a').on('click', function() {
+	$('.has-dropdown a').on('click', function () {
 		$(this).parent().toggleClass('open-drop');
 	});
 
 	///////////////////////////
 	// On Scroll
-	$(window).on('scroll', function() {
+	$(window).on('scroll', function () {
 		var wScroll = $(this).scrollTop();
 
 		// Fixed nav
@@ -79,28 +79,28 @@
 	///////////////////////////
 	// Owl Carousel
 	$('#about-slider').owlCarousel({
-		items:1,
-		loop:true,
-		margin:15,
+		items: 1,
+		loop: true,
+		margin: 15,
 		nav: true,
-		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		dots : true,
-		autoplay : true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		dots: true,
+		autoplay: true,
 		animateOut: 'fadeOut'
 	});
 
 	$('#testimonial-slider').owlCarousel({
-		loop:true,
-		margin:15,
-		dots : true,
+		loop: true,
+		margin: 15,
+		dots: true,
 		nav: false,
-		autoplay : true,
-		responsive:{
+		autoplay: true,
+		responsive: {
 			0: {
-				items:1
+				items: 1
 			},
-			992:{
-				items:2
+			992: {
+				items: 2
 			}
 		}
 	});
